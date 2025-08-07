71d32d6a204131054d2649e5b2e2bb7d4e50d0a8
Eddie Prieto <eprieto.n@gmail.com>
2025-08-07 12:32:58 -0400

    small refactor

--- wikipedia_bot.py
+++ wikipedia_bot.py
@@ -1,6 +1,6 @@
 import wikipedia
 
-input_text = "\n>>> "
+input_prompt = "\n>>> "
 
 def random():
     random_pages = wikipedia.random(pages=10)
@@ -8,65 +8,72 @@ def random():
         print(page)
 
 
-def search_pages(query):
+def get_page(query):
     titles = wikipedia.search(query, results=5)
     if not titles:
         print("I couldn't find any matching Wikipedia pages.")
         return None
 
-    print("\nPlease choose a page:")
+    print("\nChoose a page:")
     for i, title in enumerate(titles, 1):
         print(f"{i}. {title}")
 
     while True:
-            page_choice = int(input(input_text))
+            page_choice = int(input(input_prompt))
             selected_title = titles[page_choice - 1]
             page = wikipedia.page(selected_title, auto_suggest=False)
             return page
+
+def page_summary(page):
+    print(f"\n{page.title} / Summary:\n")
+    print(page.summary)
+
+def page_sections(page):
+    print(f"\n{page.title} / Sections:\n")
+    for i, section in enumerate(page.sections, 1):
+        print(f"{i}. {section}")
+    print("0. Back")
+
+def page_section(page, section_choice):
+    section = page.sections[section_choice - 1]
+    print(f"\n{page.title} : {section}\n")
+    print(page.section(section))
 
 
 def chatbot():
     while True:
         print("Enter a search term. Type 'random' for 10 random articles. To leave, type 'exit' or 'quit'.\n")
-        user_input = input(input_text)
-        if user_input.lower() in ["exit", "quit"]:
+        user_input = input(input_prompt).lower()
+        if user_input in ["exit", "quit"]:
             print("Goodbye!")
             break
-        elif user_input.lower() in ["random"]:
+        elif user_input in ["random"]:
             random()
         else:
-            page = search_pages(user_input)
-            summary = page.summary
-            sections = page.sections
+            page = get_page(user_input)
             if page:
                 while True:
-                    print("\nChoose an option:")
+                    print(f"\nPage menu for '{page.title}':")
                     print("1. Page Summary")
                     print("2. Page Sections")
                     print("3. Page Links")
                     print("4. Page content")
                     print("0. Back")
 
-                    choice = input(input_text).strip()
+                    choice = input(input_prompt).strip()
 
                     if choice == "0": # Exit to main chatbot loop for a new search
                         break  
                     elif choice == "1": # Page Summary
-                        print("Summary for '" + page.title + "':")
-                        print(summary)
+                        page_summary(page)
                     elif choice == "2": # Page sections
                         while True:
-                            print("\nSections for '" + page.title + "':")
-                            for i, section in enumerate(sections, 1):
-                                print(f"{i}. {section}")
-                            print("0. Back")
-                            section_choice = int(input(input_text))
+                            page_sections(page)
+                            section_choice = int(input(input_prompt))
                             if section_choice == 0: # Show the menu again
                                 break  
-                            elif 1 <= section_choice <= len(sections):
-                                section = sections[section_choice - 1]
-                                print(f"\n{page.title} : {section}\n")
-                                print(page.section(section))
+                            else:
+                                page_section(page, section_choice)
                                 break
                     elif choice == "3":
                         print("Links for '" + page.title + "':")
